e86030eb2adc71e958141594018d5fdf7a94acbb
aburkevics4 <aburkevics4@DX02043381.ri.riga.lv>
2020-02-26 08:57:12 +0200

    Merge origin/master
    Conflicts:
    	README.md

--- public_html/script.js
+++ public_html/script.js
@@ -5,7 +5,7 @@ function expand(){
     let ievads1 = document.createElement("INPUT"); ievads1.style="text-align: center";
     let ievads2 = document.createElement("INPUT"); ievads2.style="text-align: center";
     let poga = document.createElement("INPUT"); poga.value="novākt"; poga.type="button";
-    poga.onClick=()=>{
+    poga.onclick=()=>{
         poga.parentNode.remove();
     };
     divs.appendChild(ievads1); divs.appendChild(ievads2); divs.appendChild(poga);
