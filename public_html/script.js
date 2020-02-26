1f791979b0a5273401d4541b5f08332f3d929ab5
aburkevics4 <aburkevics4@DX02043381.ri.riga.lv>
2020-02-26 08:35:28 +0200

    Merge origin/master
    Conflicts:
    	README.md

--- public_html/script.js
+++ public_html/script.js
@@ -1,7 +1,15 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+function expand(){
+    let i=document.getElementsByClassName("line").length;
+    let divs = document.createElement("DIV"); divs.setAttribute("class","rinda");
+    divs.innerHTML (i+1)+")";
+    let ievads1 = document.createElement("INPUT"); ievads1.style="text-align: center";
+    let ievads2 = document.createElement("INPUT"); ievads2.style="text-align: center";
+    let poga = document.createElement("INPUT"); poga.value="novākt"; poga.type="button";
+    poga.onClick=()=>{
+        poga.parentNode.remove();
+    };
+    divs.appendChild(ievads1); divs.appendChild(ievads2); divs.appendChild(poga);
+    document.getElementById("forma").appendChild(divs);
+}
 
 
