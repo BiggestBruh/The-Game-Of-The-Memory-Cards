d52202367ae98abcd82c5f06fbc5696746f9bac4
aburkevics4 <aburkevics4@DX02043381.ri.riga.lv>
2020-03-04 09:19:57 +0200

    Merge origin/master
    Conflicts:
    	README.md

--- public_html/script.js
+++ public_html/script.js
@@ -1,15 +1,47 @@
-function expand(){
-    let i=document.getElementsByClassName("line").length;
-    let divs = document.createElement("DIV"); divs.setAttribute("class","rinda");
-    divs.innerHTML="test";
-    let ievads1 = document.createElement("INPUT"); ievads1.style="text-align: center";
-    let ievads2 = document.createElement("INPUT"); ievads2.style="text-align: center";
-    let poga = document.createElement("INPUT"); poga.value="novākt"; poga.type="button";
-    poga.onclick=()=>{
-        poga.parentNode.remove();
-    };
-    divs.appendChild(ievads1); divs.appendChild(ievads2); divs.appendChild(poga);
-    document.getElementById("forma").appendChild(divs);
+    var i=4;
+    var j=3;
+    
+    window.addEventListener("load", function() {
+        let buttonup = document.createElement("BUTTON");
+        let buttondown = document.createElement("BUTTON");
+        let textup = document.createTextNode("Bigger");
+        let textdown = document.createTextNode("Smaller");
+        buttonup.appendChild(textup);
+        buttondown.appendChild(textdown);
+        document.body.appendChild(buttonup);
+        document.body.appendChild(buttondown);
+        buttonup.onclick=()=>{
+        sizeup();
+        };
+        buttondown.onclick=()=>{
+        sizedown();
+        };
+        generate();
+    });
+
+function generate(){
+    for (let tempj=0; tempj<j; tempj++){
+        let divs = document.createElement("DIV"); divs.setAttribute("class","rinda");
+        for (let tempi=0; tempi<i; tempi++){
+            let pic = document.createElement("IMG");
+            pic.src = "img/RV1G.png";
+            divs.appendChild(pic);
+            document.getElementById("forma").appendChild(divs);
+            }
+        }
+        
+    }
+
+function sizeup(){
+    i=i+1;
+    j=j+1;
+    divs.parentNode.remove();
+    generate();
+}
+function sizedown(){
+    i=i-1;
+    j=j-1;
+    generate();
 }
 
 
