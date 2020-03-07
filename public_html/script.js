c4460d503f300ae38309ba4301a3f08c247b8ead
DELL <DELL@DESKTOP-8RI8UDN>
2020-03-07 21:28:47 +0200

    Merge origin/epic
    Conflicts:
    	public_html/index.html

--- public_html/script.js
+++ public_html/script.js
@@ -15,40 +15,39 @@
         "n",
         "o"
     ];
+    let pics = [];
     
-    window.addEventListener("load", function() {
-        generate(4, 3);
-    });
 
 function generate(w, h){
     document.getElementById("atminas-spele").innerHTML = "";
+    let index = 0;
     for (let temph=0; temph<h; temph++){
         let divs = document.createElement("DIV"); divs.setAttribute("class","rinda");
         for (let tempw=0; tempw<w; tempw++){
-            let pic = document.createTextNode("IMG");
-//            pic.src = "img/RV1G.png";
-//            pic.style.width = "100px";
-//            pic.style.height = "150px";
-//            pic.style.margin = "25px";
+            let card = document.createElement("DIV"); card.setAttribute("class","flip");
+            let innercard = document.createElement("DIV"); innercard.setAttribute("class", "flipinner");
+            inndercard.classList.add(pics[index].substring(0, pics[index].length - 1));
+            let backcard = document.createElement("DIV"); backcard.setAttribute("class", "flipback");
+            let frontcard = document.createElement("DIV"); frontcard.setAttribute("class", "flip-card-front");
+            let pic = document.createElement("IMG");
+            pic.src = "img/RV1G.png";
+            pic.style = "width:100%; height:100%";
+            let image = document.createElement("IMG");
+            image.src = "img/" + pics[index] + ".jpg";
+            image.style = "width:100%; height:100%";
+            cardIn.addEventListener('click', function () {
+                let cond = document.getElementsByClassName('flipped')
+                if(!cardIn.classList.contains('flipped') && !cardIn.classList.contains('fixed') && cond.length<2) {
+                    cardIn.classList.toggle('flipped');
+                    setTimeout(Match(),10);
+                }
+            });
+            
             divs.appendChild(pic);
             document.getElementById("atminas-spele").appendChild(divs);
-            }
         }
     }
+}
 
-function sizeup(){
-    if (i<6){
-    i=i+1;
-    j=j+1;
-    generate();
-    }
-}
-function sizedown(){
-    if (i>4){
-        i=i-1;
-        j=j-1;
-        generate();
-    }
-}
 
 
